3758eb6236572373d30ce084b4aebad4615bdd8f
skolodny-leffell <skolodny@student.leffellschool.org>
2024-12-09 15:44:51 -0500

    Combined temp code with SD code (#1)
    * ignore all .vscode specific files
    
    * combined temp code with SD logging code
    
    * added forgotten simeicolon
    
    * removed unneeded code
    
    * fixed pin issue
    
    ---------
    
    Co-authored-by: skolodny <spencerkolodny1@gmail.com>

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,21 @@
 #include <SD.h>
 #include <SPI.h>
 #include <DHT22.h>
-//define pin data
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is conncted to the Arduino digital pin 4
+#define INSIDE 4
+#define OUTSIDE 6
 #define pinDATA 7 // SDA, or almost any other I/O pin
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire in(INSIDE);
+OneWire out(OUTSIDE);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors_in(&in);
+DallasTemperature sensors_out(&out);
 
 DHT22 dht22(pinDATA); 
  
@@ -14,6 +27,10 @@ void setup() {
     
   Serial.begin(9600);
   pinMode(pinCS, OUTPUT);
+
+  // Start up the library
+  sensors_in.begin();
+  sensors_out.begin();
   
   // SD Card Initialization
   if (SD.begin()) {
@@ -30,7 +47,7 @@ void setup() {
   // Create/Open file 
   myFile = SD.open("test.txt", FILE_WRITE);
   
-  myFile.print("Date | Time | Humidity | Temperature");
+  myFile.print("Date | Time | Humidity | Temperature | Inside Temperature | Outside Temperature");
   myFile.flush();
 }
 
@@ -38,19 +55,23 @@ void loop() {
   float t = dht22.convertCtoF(dht22.getTemperature());
   float h = dht22.getHumidity();
 
-  if (dht22.getLastError() != dht22.OK) {
-    Serial.print("last error :");
-    Serial.println(dht22.getLastError());
-  }
+  sensors_in.requestTemperatures();
+  float insideCelsius = sensors_in.getTempCByIndex(0);
+  delay(10);
+  sensors_out.requestTemperatures();
+  float outsideCelisus = sensors_out.getTempCByIndex(0);
 
     // if the file opened okay, write to it:
   if (myFile) {
     // Write to file
-    
     myFile.println();
     myFile.print(h, 1);
     myFile.print(" | ");
     myFile.print(t, 1);
+    myFile.print(" | ");
+    myFile.print(insideCelsius);
+    myFile.print(" | ");
+    myFile.print(outsideCelisus);
     myFile.flush();                                               
   } else { // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
