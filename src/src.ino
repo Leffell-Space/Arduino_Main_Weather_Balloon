a47ed8d630892a5511759b80fb6bb8a371c16954
skolodny-leffell <skolodny@student.leffellschool.org>
2024-12-16 15:32:45 -0500

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -3,6 +3,12 @@
 #include <DHT22.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <TinyGPS++.h> // test
+#include <SoftwareSerial.h>
+
+TinyGPSPlus gps;
+
+SoftwareSerial ss(4, 3); // RX, TX pins (adjust as needed)
 
 // Data wire is conncted to the Arduino digital pin 4
 #define INSIDE 4
@@ -28,6 +34,8 @@ String dataFile = "data.csv";
 void setup() {
     
   Serial.begin(9600);
+  ss.begin(9600); // GPS baud rate
+
   pinMode(pinCS, OUTPUT);
 
   // Start up the library
@@ -49,11 +57,33 @@ void setup() {
   // Create/Open file 
   myFile = SD.open(dataFile, FILE_WRITE);
   
-  myFile.print("Date,Time,Humidity,Temperature,Inside Temperature,Outside Temperature");
+  myFile.print("Time,Latitude,Longitude,Altitude,Satellite Count,HDOP,Humidity,Temperature,Inside Temperature,Outside Temperature");
   myFile.flush();
 }
 
 void loop() {
+  if (ss.available() > 0) {
+    gps.encode(ss.read()); // Feed data to the GPS library
+   if (gps.location.isUpdated()) {
+      // Get location information
+      double latitude = gps.location.lat();
+      double longitude = gps.location.lng();
+      double altitude = gps.altitude.meters(); // Altitude in meters
+
+      // Get the number of satellites in view
+      int satelliteCount = gps.satellites.value();
+
+      // Get the timestamp (in hours, minutes, seconds)
+      unsigned long timestamp = gps.time.value();
+      int hours = gps.time.hour();
+      int minutes = gps.time.minute();
+      int seconds = gps.time.second();
+
+      // Get the Horizontal Dilution of Precision (HDOP)
+      float hdop = gps.hdop.value() / 100.0; // HDOP is returned in hundredths, so divide by 100
+   } 
+  }
+ 
   float t = dht22.convertCtoF(dht22.getTemperature());
   float h = dht22.getHumidity();
 
@@ -66,6 +96,16 @@ void loop() {
     // if the file opened okay, write to it:
   if (myFile) {
     // Write to file
+    if (gps.location.isUpdated()) { 
+      myFile.print(hours); myFile.print(":");
+      myFile.print(minutes); myFile.print(":");
+      myFile.print(seconds); myFile.print(",");
+      myFile.print(latitude, 6); myFile.print(",");
+      myFile.print(longitude, 6); myFile.print(",");
+      myFile.print(altitude, 1); myFile.print(",");
+      myFile.print(satelliteCount); myFile.print(",");
+      myFile.print(hdop, 2); myFile.print(",");
+    }
     myFile.println();
     myFile.print(h, 1);
     myFile.print(",");
