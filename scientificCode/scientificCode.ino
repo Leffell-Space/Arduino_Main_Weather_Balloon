4b4f1faeedfec4bd8736e79300ac94edd17fbf3a
skolodny <121265165+skolodny@users.noreply.github.com>
2025-02-05 11:53:06 -0500

    Scientific sensors (#28)

--- scientificCode/scientificCode.ino
+++ scientificCode/scientificCode.ino
@@ -3,6 +3,14 @@
 MS5611 baro;
 int32_t pressure;
 float filtered = 0;
+int sensorValue = 0;
+const int AQPin = 0;
+float volts;
+
+const int R0 = 346.76;
+const int R2 = 1965;
+float RS;
+float PPM_acetone;
 
 void setup() {
   // Start barometer
@@ -16,13 +24,28 @@ void setup() {
 void loop() {
   // Read pressure
   pressure = baro.getPressure();
+  sensorValue = analogRead(AQPin); 
+
+  volts = sensorValue * 5;
+  volts = volts / 1023;  
   
   if(filtered != 0){
     filtered = filtered + 0.1*(pressure-filtered);
-  }
-  else {
+  } else {
     filtered = pressure;          // first reading so set filtered to reading
   }
+
+  // calculate RS
+   RS = R2 * (1-volts);
+   RS = RS/volts;
+   // calculate acetone PPM
+   PPM_acetone = 159.6 - 133.33*(RS/R0);
+   // print out the acetone concentration:
+
   // Send pressure via serial (UART);
-  Serial.println("Pressure: " + String(filtered));
+  Serial.print("Pressure: " + String(filtered) + " | ");
+  Serial.print("Air Quality: " + String(PPM_acetone) + " | ");
+  Serial.println("Volts: " + String(volts));
+
+  delay(1);
 }
