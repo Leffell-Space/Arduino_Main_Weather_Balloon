bc21c46761caa871c0f556ea0b42fb5c095b2914
skolodny <spencerkolodny1@gmail.com>
2025-03-31 15:26:50 -0400

    Add buzzer functionality and improve code formatting

--- scientificCode/scientificCode.ino
+++ scientificCode/scientificCode.ino
@@ -3,6 +3,8 @@
 
 #define COLLECT_NUMBER  20              // collect number, the collection range is 1-100
 #define Ozone_IICAddress OZONE_ADDRESS_3
+#define BUZZER_PIN 9  // Define buzzer pin
+
 DFRobot_OzoneSensor Ozone;
 
 MS5611 baro;
@@ -34,7 +36,9 @@ void setup() {
    * MEASURE_MODE_AUTOMATIC         active  mode
    * MEASURE_MODE_PASSIVE           passive mode
    */
-    Ozone.setModes(MEASURE_MODE_PASSIVE);
+  Ozone.setModes(MEASURE_MODE_PASSIVE);
+
+  pinMode(BUZZER_PIN, OUTPUT);
 
   delay(2);
 }
@@ -43,12 +47,10 @@ void loop() {
   // Read pressure
   pressure = baro.getPressure();
   sensorValue = analogRead(AQPin); 
-
   volts = sensorValue * 5;
   volts = volts / 1023;  
-  
   if(filtered != 0){
-    filtered = filtered + 0.1*(pressure-filtered);
+    filtered = filtered + 0.1 * (pressure - filtered);
   } else {
     filtered = pressure;          // first reading so set filtered to reading
   }
@@ -56,16 +58,28 @@ void loop() {
   // calculate RS
    RS = R2 * (1-volts);
    RS = RS/volts;
-   // calculate acetone PPM
-   PPM_acetone = 159.6 - 133.33*(RS/R0);
+  // calculate acetone PPM
+  PPM_acetone = 159.6 - 133.33 * (RS / R0);
 
-   //Get the ozone concentration
    int16_t ozoneConcentration = Ozone.readOzoneData(COLLECT_NUMBER);
 
-   // Send pressure via serial (UART);
+   // Calculate altitude (assuming sea level pressure is 1013.25 hPa)
+   float altitude = 44330.0 * (1.0 - pow(filtered / 101325.0, 0.1903));
+
    Serial.print("Pressure: " + String(filtered) + " | ");
+   //Serial.print(String(pow((filtered / 101325.0), 0.1903)));
+   Serial.print("Altitude: " + String(altitude) + " ft | ");
    Serial.print("Air Quality: " + String(PPM_acetone) + " | ");
    Serial.print("Volts: " + String(volts) + " | ");
    Serial.println("Ozone: " + String(ozoneConcentration) + " PPB.");
-   delay(1);
+
+
+  if (altitude < 300) {
+    digitalWrite(BUZZER_PIN, HIGH);
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+  
+  
+  delay(1);
 }
