37dfe8b54f9fe2bd25ce59b546d359d8cdacba85
skolodny <spencerkolodny1@gmail.com>
2025-05-13 08:38:27 -0400

    added working code

--- src/src.ino
+++ src/src.ino
@@ -1,4 +1,3 @@
-#include <SD.h>
 #include <SPI.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -7,14 +6,12 @@
 #include <Wire.h>
 #include <MS5611.h>
 #include "DFRobot_OzoneSensor.h"
-//#include <SensirionI2cScd30.h>
+#include <SensirionI2cScd30.h>
 
 TinyGPSPlus gps;
 
-SoftwareSerial ss(4, 3);  // RX, TX pins (adjust as needed)
-
-#define INSIDE 5
-#define OUTSIDE 6
+#define INSIDE 5   //inside temp
+#define OUTSIDE 6  //outside temp
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire in(INSIDE);
@@ -30,48 +27,33 @@ String dataFile = "data.csv";
 
 #define COLLECT_NUMBER 20  // collect number, the collection range is 1-100
 #define Ozone_IICAddress OZONE_ADDRESS_3
-//SensirionI2cScd30 sensor;
+SensirionI2cScd30 sensor;
 
 #define BUZZER_PIN 9  // Define buzzer pin
 
-//DFRobot_OzoneSensor Ozone;
+DFRobot_OzoneSensor Ozone;
 
 MS5611 baro;
 int32_t pressure;
 float filtered = 0;
 float co2Concentration = 0;
 float temperature = 0;
-float humidity = 0; 
+float humidity = 0;
+
+double latitude;
+double longitude;
+double altitude;
+int hours;
+int minutes;
+int seconds;
+int previousMillis = 0;
 
 void setup() {
-  // Start barometer
-  baro = MS5611();
-  baro.begin();
-  // Start serial (UART)
   Serial.begin(9600);
-  /*
-  if (!Ozone.begin(Ozone_IICAddress)) {
-    Serial.println("Ozone sensor I2c device number error !");
-  } else {
-    Serial.println("Ozone sensor working");
-  }
-  */
-  //Ozone.setModes(MEASURE_MODE_PASSIVE);
-
-  ss.begin(9600);  // GPS baud rate
-
-  // Start up the library
-  sensors_in.begin();
-  sensors_out.begin();
-  
-    pinMode(BUZZER_PIN, OUTPUT);
-  //sensor.begin(Wire, SCD30_I2C_ADDR_61);
-  //sensor.startPeriodicMeasurement(0);
-
-  // SD Card Initialization
-  if (!SD.begin(10)) {
+  Serial1.begin(9600);
+    // SD Card Initialization
+  if (!SD.begin(53)) {
     Serial.println("initialization failed!");
-    while (1);
   }
   //Serial.println("initialization done.");
 
@@ -85,51 +67,43 @@ void setup() {
   // Create/Open file
   myFile = SD.open(dataFile, FILE_WRITE);
   //Serial.println(myFile.position());
-  myFile.println("Time,Latitude,Longitude,Altitude,Satellite Count,HDOP,Inside Temperature,Outside Temperature,Pressure,PPM Acetone,Air Quality,Ozone Concentration");
-  //Serial.println(myFile.position());
+  myFile.println("Time,Lat,Long,Alt,Inside Temp,Outside Temp,Pressure,Ozone Concentration,CO2 Quality,Temperature,Humidity");
   myFile.flush();
   //Serial.println("RAN");
+  
+  // Start barometer
+  baro = MS5611();
+  baro.begin();
+  
+  // Start serial (UART)
 
-  return;
+  if (!Ozone.begin(Ozone_IICAddress)) {
+    Serial.println("Ozone sensor I2c device number error !");
+  } else {
+    Serial.println("Ozone sensor working");
+  }
+  Ozone.setModes(MEASURE_MODE_PASSIVE);
+  
+
+  // Start up the library
+  sensors_in.begin();
+  sensors_out.begin();
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  sensor.begin(Wire, SCD30_I2C_ADDR_61);
+  sensor.startPeriodicMeasurement(0);
 }
 
 void loop() {
-  
-  double latitude;
-  double longitude;
-  double altitude;
-  int satelliteCount;
-  int hours;
-  int minutes;
-  int seconds;
-  float hdop;
-  
-    // Read pressure
-  pressure = baro.getPressure();
 
-  if (filtered != 0) {
-    filtered = filtered + 0.1 * (pressure - filtered);
-  } else {
-    filtered = pressure;  // first reading so set filtered to reading
-  }
-  
-  //int16_t ozoneConcentration = Ozone.readOzoneData(COLLECT_NUMBER);
-  //sensor.blockingReadMeasurementData(co2Concentration, temperature, humidity);  
-
-  if (altitude < 300) {
-    digitalWrite(BUZZER_PIN, HIGH);
-  } else {
-    digitalWrite(BUZZER_PIN, LOW);
-  }
-  
-  //Serial.println("BREAK 1");
-  while (ss.available() > 0) {
-    gps.encode(ss.read());  // Feed data to the GPS library
+  // Implement your timestamp logic here
+  while (Serial1.available() > 0) {
+    gps.encode(Serial1.read());  // Feed data to the GPS library
     //Serial.println(gps.location.isUpdated());
     if (gps.location.isUpdated()) {
       //Serial.println("BREAK 3");
+
       // Get location information
-      
       latitude = gps.location.lat();
       longitude = gps.location.lng();
       altitude = gps.altitude.meters();  // Altitude in meters
@@ -138,46 +112,58 @@ void loop() {
       hours = gps.time.hour();
       minutes = gps.time.minute();
       seconds = gps.time.second();
-
-      sensors_in.requestTemperatures();
-      float insideCelsius = sensors_in.getTempCByIndex(0);
-      sensors_out.requestTemperatures();
-      float outsideCelsius = sensors_out.getTempCByIndex(0);
-
-      // if the file opened okay, write to it:
-      // Write to file
-      myFile.print(hours);
-      myFile.print(":");
-      myFile.print(minutes);
-      myFile.print(":");
-      myFile.print(seconds);
-      myFile.print(",");
-      myFile.print(latitude, 6);
-      myFile.print(",");
-      myFile.print(longitude, 6);
-      myFile.print(",");
-      myFile.print(altitude, 3);
-      myFile.print(",");
-      myFile.print(insideCelsius);
-      myFile.print(",");
-      myFile.print(outsideCelsius);
-      myFile.print(",");
-      myFile.print(filtered);
-      myFile.print(",");
-      //myFile.print(ozoneConcentration);
-      myFile.print(",");
-      myFile.print(co2Concentration);
-      myFile.print(",");
-      myFile.print(temperature);
-      myFile.print(",");
-      myFile.print(humidity);
-      myFile.print(",");
-      myFile.flush();
-      Serial.println(myFile.position());
-      delay(1000);
-      
     }
-    
   }
-  
+
+    // Read pressure
+    pressure = baro.getPressure();
+
+    int16_t ozoneConcentration = Ozone.readOzoneData(COLLECT_NUMBER);
+    sensor.blockingReadMeasurementData(co2Concentration, temperature, humidity);
+
+
+    //Serial.println("BREAK 1");
+
+
+    sensors_in.requestTemperatures();
+    float insideCelsius = sensors_in.getTempCByIndex(0);
+    sensors_out.requestTemperatures();
+    float outsideCelsius = sensors_out.getTempCByIndex(0);
+
+    if (altitude < 300) {
+      digitalWrite(BUZZER_PIN, HIGH);
+    } else {
+      digitalWrite(BUZZER_PIN, LOW);
+    }
+
+
+    // if the file opened okay, write to it:
+    // Write to file
+    float data[13] = {hours, minutes, seconds, latitude, longitude, altitude, insideCelsius, outsideCelsius, pressure, ozoneConcentration, co2Concentration, temperature, humidity};
+
+    int currentMillis = millis();
+    if (currentMillis - previousMillis >= 10000) {  //write to SD card only every 10 seconds
+      previousMillis = currentMillis;
+      myFile = SD.open(dataFile, FILE_WRITE);
+      if (myFile) {
+        // use a for loop to concatante the array into a string
+        String allData = "";        //write one long string
+        for (int i = 0; i < 13; i++) {
+          if (i < 2) {
+            allData += String(data[i]) + ":";
+          } else if (i == 12) {
+            allData += String(data[i]);
+          } else {
+            allData += String(data[i]) + ",";
+          }
+          Serial.print(allData);
+          Serial.print(", i: ");
+          Serial.println(i);
+        }
+        Serial.println(allData);
+        myFile.println(allData);
+        myFile.flush();
+        myFile.close();
+      }
+    }
 }
